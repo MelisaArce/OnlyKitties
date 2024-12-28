d87894233c67e3a7dfc5a620861327ea9b198133
natashafl <florentin.natasha@gmail.com>
2024-12-28 11:50:18 -0300

    Authcontext (#5)
    * Mensaje del commit
    
    * reubicación de los archivos del header
    
    * edicion de logo, mejorado, e importe el header al app.jsx
    
    * borre comentarios de mas
    
    * formulario de registro
    
    * Agregue context, y usestate a register
    
    * corregi unos errores
    
    * cambios en header y registro
    
    * solucione unas cositas del  register
    
    ---------
    
    Co-authored-by: Melisa Arce <95306750+MelisaArce@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import Home from './page/Home/Home';
-import Header from './components/header/Header';
+import Header from './components/header/headern/Header';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
+import { AuthProvider } from './components/context/authcontext';
+import Registro from './page/register/register';
 
 function App() {
   return (
-        <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="crear-publicacion" element={<Home />} />
+          <Route path="modificar-cuenta/:idUser" element={<Home />} />
+          <Route path="register" element={<Registro />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
-export default App
+export default App;
