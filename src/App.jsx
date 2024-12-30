8d570287b52bba70318cd76ff606bca4d18ce150
MelisaArce <melisaamelia.arce@gmail.com>
2024-12-30 13:10:56 -0300

    actualizacion del login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/context/authcontext';
+
 import Home from './page/Home/Home';
 import Login from './page/login/Login';
 import Header from './components/header/headern/Header';
 import Footer from './components/footer/Footer';
-import "./App.css";
-import { AuthProvider } from './components/context/authcontext';
 import Registro from './page/register/register';
+import "./App.css";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="crear-publicacion" element={<Home />} />
           <Route path="modificar-cuenta/:idUser" element={<Home />} />
-          <Route path="register" element={<Registro />} />
+          <Route path="/register" element={<Registro />} />
           <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
