662abff61c157084ec13f2bc33af83f23657ae88
Melisa Arce <95306750+MelisaArce@users.noreply.github.com>
2024-12-30 12:24:39 -0300

    Login (#6)
    * cambios en el footer
    
    * component login
    
    * ruta login
    
    * agrego de hook en login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './page/Home/Home';
+import Login from './page/login/Login';
 import Header from './components/header/headern/Header';
 import Footer from './components/footer/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import { AuthProvider } from './components/context/authcontext';
 import Registro from './page/register/register';
@@ -16,6 +18,7 @@ function App() {
           <Route path="crear-publicacion" element={<Home />} />
           <Route path="modificar-cuenta/:idUser" element={<Home />} />
           <Route path="register" element={<Registro />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
