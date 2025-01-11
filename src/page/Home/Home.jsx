a50574a65f692abe7d0af9efa0b3d2b0fbfec03c
Melisa Arce <95306750+MelisaArce@users.noreply.github.com>
2025-01-11 16:04:51 -0300

    Entidad/principal (#9)
    * cambios en el footer
    
    * component login
    
    * ruta login
    
    * agrego de hook en login
    
    * actualizacion del login
    
    * primeros avances de la entidad principal, y algunas correciones menores
    
    * creacion de la entidad gatitos, creacion de mock para visualizacion de las cards
    
    * actualizacion de entidad gatitos

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,13 +1,24 @@
+import React from "react";
+import gatitosMock from "../../util/mockData";
+import GatitoCard from "../../components/gatitos/GatitoCard";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <h1>Only Kitties</h1>
+      <h2>Gatitos en Adopción</h2>
+      <div className="gatitos-grid">
+        {gatitosMock.map((gatito) => (
+          <GatitoCard key={gatito.id} gatito={gatito} />
+        ))}
       <h4>Quieres adoptar un gatito?, haz click en el botón de abajo</h4>
       <Link to="/adoptar-gatito">
         <button>Adoptar un gatito</button>
       </Link>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
