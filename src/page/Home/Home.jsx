2be1ed7bb9129ea06e9d815b46e42854517e3270
MelisaArce <melisaamelia.arce@gmail.com>
2025-01-12 22:40:38 -0300

    correccion de la paginacion en el home

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import gatitosMock from "../../util/mockData";
-import GatitoCard from "../../components/gatitos/GatitoCard";
 import { Link } from "react-router-dom";
+import gatitosMock from "../../util/mockData";
+import GatitoList from "../../components/gatitos/GatitoList";
 import "./home.css";
 
 const Home = () => {
@@ -9,10 +9,8 @@ const Home = () => {
     <div className="home">
       <h1>Only Kitties</h1>
       <h2>Gatitos en Adopción</h2>
-        <div className="gatitos-grid">
-          {gatitosMock.map((gatito) => (
-            <GatitoCard key={gatito.id} gatito={gatito} />
-          ))}
+        <div>
+          <GatitoList />
         </div>
       <h4>Quieres adoptar un gatito?, haz click en el botón de abajo</h4>
       <Link to="/register">
