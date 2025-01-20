bafeee5804e67284f6969faa0766e2639ce460df
MelisaArce <melisaamelia.arce@gmail.com>
2025-01-20 11:37:22 -0300

    se modifico el AuthContext para que no se cierre la sesion del usuario, a su vez esto tuvo que modificar el login y el header

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,19 +1,27 @@
-import { createContext, useState } from "react";
-
+import React, { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
-
 export const AuthProvider = ({ children }) => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(() => {
+    // Leer el estado inicial desde localStorage
+    const storedLogin = localStorage.getItem("isLogin");
+    return storedLogin ? JSON.parse(storedLogin) : false;
+  });
+
+  const login = () => {
+    setIsLogin(true);
+    localStorage.setItem("isLogin", JSON.stringify(true)); // Guardar estado en localStorage
+  };
+
+  const logout = () => {
+    setIsLogin(false);
+    localStorage.removeItem("isLogin"); // Eliminar estado de localStorage
+  };
 
   return (
-    <AuthContext.Provider value={{ isLogin, setIsLogin }}>
+    <AuthContext.Provider value={{ isLogin, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
-
-
-
-
